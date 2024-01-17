6615b358e8b0856bf6d80670b78b7435b664710a
talia <talia.van.den.bossche@student.howest.be>
2024-01-17 12:25:15 +0100

    data op goeie manier

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,45 @@
-const getData = function(){
-    handleData(`../data/data.json`, showData, showError)
+
+const showLeaderboard = function () {
+
+};
+
+const getLeaderboard = function(data){
+    console.info(data.spelers);
+    console.info(data.metingen);
+    let metingen = data.metingen;
+    let spelers = data.spelers;
+
+    metingen.sort(function(a, b) {
+        return b.afstand - a.afstand;
+    });
+    console.log(JSON.stringify(data, null, 2));
+    
+    metingen.forEach(meting => {
+        let speler = spelers.find(speler => speler.id === meting.speler_id);
+        let sortedJson = {
+            "naam": speler.voornaam + ' ' + speler.achternaam.charAt(0) + '.',
+            "afstand": meting.afstand,
+            "snelheid": meting.maxSnelheid,
+        }
+        console.log(sortedJson);
+    });
 }
 
-const showData = function(data){
-    console.log(data);
+
+function fetchLeaderboardData() {
+  fetch('./data/data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      // You can use the JSON data here
+      console.log(data);
+      console.log('fetch done');
+    getLeaderboard(data);
+    })
+    .catch((error) => {
+      console.error('An error occurred while fetching JSON data: ' + error);
+    });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchLeaderboardData();
+});
