6c9c7ee091a5d85a99b5418c2c793e15dad40b30
xevisa <xevisagames@gmail.com>
2024-01-30 06:12:46 +0100

    Connection screen integration
    Started working on connection screen, awaiting testing and also fixed some bugs

--- Frontend/scripts/scriptScreen.js
+++ Frontend/scripts/scriptScreen.js
@@ -326,14 +326,12 @@ function startCountdown(duration, callback, showFinalCountdown = false) {
     : null;
 
   let timerId = setInterval(() => {
-    if(timeLeft === 3){
-      socketio.emit('FS2B_play_countdownmuziek')
-    }
-    else if (timeLeft > 0) {
+    if (timeLeft > 0) {
       console.log(timeLeft + ' seconds remaining');
 
       // Only update the aftelElement if showFinalCountdown is true and timeLeft is 3 or less
       if (showFinalCountdown && timeLeft <= 3.5) {
+        socketio.emit('FS2B_play_countdownmuziek')
         document.querySelector('.js-spring').style.display = 'none';
         countdownElement.style.display = 'none'; // Hide the countdown
         aftelElement.style.display = 'flex'; // Show the countdown
